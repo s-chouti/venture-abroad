10c15457a13df9794fdba30fc83095834dd3053a
shravan-chouti <shravan@ratiopayments.com>
2025-03-03 13:28:18 -0500

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,9 +4,9 @@ export let seo = {
 	title: 'Venture Abroad',
 	description:
 		'Venture Abroad is a platform that connects entrepreneurs with investors and provides them with the resources they need to succeed.',
-	image: 'https://i.pinimg.com/736x/85/9a/92/859a92a2629f912010a0a72270aefedc.jpg',
+	image: 'https://venture-abroad.vercel.app/_app/immutable/assets/image-ventureabroad.BNJoLF8H.webp',
 	twitter: 'SEO twitter',
-	url: 'https://startup-sve.vercel.app',
+	url: 'https://venture-abroad.vercel.app/',
 	keywords: 'Venture Abroad, Startup, Entrepreneur, Investor',
 };
 
